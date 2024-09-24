c9ac964ae596acdaef1ce60c8b11f9a22429e574
Aditya Khobragade <adityakhobragade05@gmail.com>
2024-09-24 08:52:16 +0530

    code added 24-09-24

--- reactProject/src/components/index.js
+++ reactProject/src/components/index.js
@@ -5,13 +5,13 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import Signup from "./Signup";
 import Login from "./Login";
 import Button from "./Button";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
 import Input from "./Input";
+import SignUp from "./Signup"
 
 export {
     Header,
@@ -20,7 +20,7 @@ export {
     Logo,
     LogoutBtn,
     RTE,
-    Signup,
+    SignUp,
     Login,
     Button,
     PostForm,
